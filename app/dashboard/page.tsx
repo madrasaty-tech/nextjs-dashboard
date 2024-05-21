aff79970d97ba079d77e82d6bf0cd9d8c4513f5c
solomon <samysam204o@gmail.com>
2024-05-21 14:28:45 +0300

    added the RevenueChart to the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,31 @@
-export default function Page() {
-  return <h1>dashboard page</h1>;
+import { lusitana } from '../ui/fonts';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { Card } from '../ui/dashboard/cards';
+import { fetchRevenue } from '../lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card titl="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card
+                    title="Total Customers"
+                    value={numberOfCustomers}
+                    type="customers"
+                  /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        {/* <LatestInvoices latestInvoices={latestInvoices}/> */}
+      </div>
+    </main>
+  );
 }
