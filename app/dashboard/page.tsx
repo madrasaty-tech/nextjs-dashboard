26ccb9f7aede0eb74f95a9090c3f6aa877fb218d
solomon <samysam204o@gmail.com>
2024-05-23 14:07:31 +0300

    use loading.tsx file to load overview skeleton

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -17,7 +17,7 @@ export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
+        Dashboard is here
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
