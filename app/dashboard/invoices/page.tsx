486baaa8251bb98f184f9e8ec054022905c3f308
solomon <samysam204o@gmail.com>
2024-06-07 10:40:04 +0300

    deploy

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <h1>invoices page</h1>;
+import { lusitana } from '@/app/ui/fonts';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Search from '@/app/ui/search';
+ 
+export default async function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
