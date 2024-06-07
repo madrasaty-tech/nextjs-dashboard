349009a6d7159919e0f42b41702ed5e41da167df
solomon <samysam204o@gmail.com>
2024-06-07 11:08:41 +0300

    deploy

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,19 +10,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    console.log(searchParams);
-    console.log(term);
-
-    const param = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams);
 
     if (term) {
-      param.set('q', term);
+      params.set('qeury', term);
 
     } else {
-      param.delete('q');
+      params.delete('qeury');
     }
 
-    replace(`${pathname}?${param.toString()}`);
+    replace(`${pathname}?${params.toString()}`);
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -33,6 +30,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
